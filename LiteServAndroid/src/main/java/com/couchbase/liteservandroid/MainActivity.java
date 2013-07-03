3c3eb70fe7558330f363c1af6cb40026190f68f0
Traun Leyden <traun@couchbase.com>
2013-07-03 15:41:06 -0700

    startup liteserv code

--- LiteServAndroid/src/main/java/com/couchbase/liteservandroid/MainActivity.java
+++ LiteServAndroid/src/main/java/com/couchbase/liteservandroid/MainActivity.java
@@ -6,24 +6,61 @@ import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 
+import com.couchbase.cblite.CBLDatabase;
+import com.couchbase.cblite.CBLServer;
+import com.couchbase.cblite.listener.CBLListener;
+
+import java.io.IOException;
+
 public class MainActivity extends Activity {
 
+    private static final int DEFAULT_LISTEN_PORT = 5986;
+    private static final String DATABASE_NAME = "cblite-test";
+    private static final String LISTEN_PORT_PARAM_NAME = "listen_port";
     public static String TAG = "LiteServ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showListenPort();
+        showListenPort(getListenPort());
+        startCBLListener(getListenPort());
     }
 
-    private void showListenPort() {
-        int listenPort = getIntent().getIntExtra("listen_port", 5986);
+    private void showListenPort(int listenPort) {
         Log.d(TAG, "listenPort: " + listenPort);
         TextView listenPortTextView = (TextView)findViewById(R.id.listen_port_textview);
-        listenPortTextView.setText("Listening on port: " + listenPort);
+        listenPortTextView.setText(String.format("Listening on port: %d.  Db: %s", listenPort, DATABASE_NAME));
     }
 
+    private void startCBLListener(int listenPort) {
+
+        CBLServer server = startCBLite();
+        startDatabase(server, DATABASE_NAME);
+
+        CBLListener listener = new CBLListener(server, listenPort);
+        Thread thread = new Thread(listener);
+        thread.start();
+    }
+
+    protected CBLServer startCBLite() {
+        CBLServer server;
+        try {
+            server = new CBLServer(getFilesDir().getAbsolutePath());
+        } catch (IOException e) {
+           throw new RuntimeException(e);
+        }
+        return server;
+    }
+
+    protected void startDatabase(CBLServer server, String databaseName) {
+        CBLDatabase database = server.getDatabaseNamed(databaseName, true);
+        database.open();
+    }
+
+    private int getListenPort() {
+        return getIntent().getIntExtra(LISTEN_PORT_PARAM_NAME, DEFAULT_LISTEN_PORT);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
