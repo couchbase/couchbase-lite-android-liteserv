ded5f7df444b73da1a0464a23dbde48b75afc2b1
Phil Labee <philip@couchbase.com>
2014-03-18 18:06:43 -0700

    CBLT-235:  some DEBUG output
    Change-Id: I50bc138de2d2d1fceb8b8e93b974c0323de97857

--- release/zip_jars.sh
+++ release/zip_jars.sh
@@ -8,7 +8,8 @@ REVISION=$1
 
 pushd ${THIS_DIR}  2>&1 > /dev/null
 
-mvn --settings ./settings.xml --quiet -DREVISION=${REVISION} clean package
+# mvn --settings ./settings.xml --quiet -DREVISION=${REVISION} clean package
+  mvn --settings ./settings.xml   -X    -DREVISION=${REVISION} clean package
 
 popd               2>&1 > /dev/null
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ done making android_zipfile
