1a2518ed7aea2586dc0caa016a1eabda2f7df91f
Phil Labee <philip@couchbase.com>
2014-03-31 16:03:38 -0700

    CBLT-252:  remove ZIP creation from build.gradle, now using these env.vars:   MAVEN_UPLOAD_VERSION,  MAVEN_UPLOAD_REPO_URL   MAVEN_UPLOAD_USERNAME, MAVEN_UPLOAD_PASSWORD
    Change-Id: Ic5e548e7bc54ac97dc90a36e7e602e2aef73f1dd

--- extra/jenkins_build/build_automation.rb
+++ extra/jenkins_build/build_automation.rb
@@ -2,10 +2,10 @@
 require 'fileutils'
 
 def uploadArchives() 
-  uploadArchivesSingleLibrary("libraries:couchbase-lite-java-core", "build", "")
-  uploadArchivesSingleLibrary("libraries:couchbase-lite-android", "build", "buildAndroidWithArtifacts")
+  uploadArchivesSingleLibrary("libraries:couchbase-lite-java-core",       "build",    "")
+  uploadArchivesSingleLibrary("libraries:couchbase-lite-android",         "build",    "buildAndroidWithArtifacts")
   uploadArchivesSingleLibrary("libraries:couchbase-lite-java-javascript", "assemble", "buildJavascriptWithArtifacts")
-  uploadArchivesSingleLibrary("libraries:couchbase-lite-java-listener", "assemble", "buildListenerWithArtifacts")
+  uploadArchivesSingleLibrary("libraries:couchbase-lite-java-listener",   "assemble", "buildListenerWithArtifacts")
 
 end
 
@@ -85,53 +85,3 @@ def runCommandCheckError(cmd)
 
 end
 
-
-def buildZipArchiveRelease() 
-  
-  android_VERSION  = ENV["VERSION"]
-  android_REVISION = ENV["REVISION"]
-  thirdPartyArchive    = "com.couchbase.cblite-#{android_REVISION}"
-  thirdPartyZipArchive = "#{thirdPartyArchive}-android.zip"
-  localArchive         = "cblite_android_#{android_REVISION}"
-  localZipArchive      = "#{localArchive}.zip"
-  
-  # create localarchive directory
-  runCommand "mkdir #{localArchive}"
-
-  # download 3rd party jars into a zip file
-  runCommand "cd    release && ./zip_jars.sh #{android_REVISION}"
-  runCommand "file  release/target/#{thirdPartyZipArchive} || exit 99"
-  runCommand "cp    release/target/#{thirdPartyZipArchive} ."
-  
-  # unzip it
-  runCommand "unzip #{thirdPartyZipArchive}"
-  
-  # rename it
-  runCommand "mv #{thirdPartyArchive} #{localArchive}"
-  
-  # collect the new cblite jar files and name them correctly based on UPLOAD_VERSION_CBLITE env var
-
-  modulesToPaths = {
-    "couchbase-lite-java-core" => "libraries/couchbase-lite-java-core/build/libs/couchbase-lite-java-core.jar",
-    "couchbase-lite-android" => "libraries/couchbase-lite-android/build/bundles/release/classes.jar",
-    "couchbase-lite-java-javascript" => "libraries/couchbase-lite-java-javascript/build/libs/couchbase-lite-java-javascript.jar",
-    "couchbase-lite-java-listener" => "libraries/couchbase-lite-java-listener/build/libs/couchbase-lite-java-listener.jar"
-  }
-
-  modulesToPaths.each { |mod, srcPath| 
-    envVarName = "UPLOAD_VERSION_CBLITE"
-    if mod == "couchbase-lite-java-javascript"
-      envVarName = "UPLOAD_VERSION_CBLITE_JAVASCRIPT"
-    elsif mod == "couchbase-lite-java-listener"
-      envVarName = "UPLOAD_VERSION_CBLITE_LISTENER"
-    end
-    envVarValue = ENV[envVarName]
-    dest = "#{localArchive}/#{mod}-#{envVarValue}.jar"     
-    cmd = "cp #{srcPath} #{dest}"
-    runCommand cmd
-  }
-  
-  # re-zip the zip file and put in current directory  
-  runCommand "zip -r --junk-paths #{localZipArchive} #{localArchive}"
-  
-end
--- extra/jenkins_build/upload_android_artifacts.sh
+++ extra/jenkins_build/upload_android_artifacts.sh
@@ -6,11 +6,10 @@ def verifyEnvVariable(envVarName)
   raise "Neet do set #{envVarName}" if ENV[envVarName] == nil 
 end
 
-verifyEnvVariable("UPLOAD_MAVEN_REPO_URL")
-verifyEnvVariable("UPLOAD_USERNAME")
-verifyEnvVariable("UPLOAD_PASSWORD")
-verifyEnvVariable("UPLOAD_VERSION_CBLITE")
-verifyEnvVariable("UPLOAD_VERSION_CBLITE_JAVASCRIPT")
+verifyEnvVariable("MAVEN_UPLOAD_VERSION")
+verifyEnvVariable("MAVEN_UPLOAD_USERNAME")
+verifyEnvVariable("MAVEN_UPLOAD_PASSWORD")
+verifyEnvVariable("MAVEN_UPLOAD_REPO_URL")
 
 clean()
 uploadArchives()
