07f8c378c1b24c4348f6e9baeb1fbbbff1d1eada
Phil Labee <philip@couchbase.com>
2014-04-10 18:37:57 -0700

    CBLT-260, CBLT-261:  TD and RevID collators in cbl_collator jar
    Change-Id: I860345a555614a70a20588105b982156f3bd73a9

--- release/zip_jars.sh
+++ release/zip_jars.sh
@@ -2,7 +2,7 @@
 
 THIS_DIR=`dirname $0`
 
-if [[ ! $1 ]] ; then echo "usage:  $0  build_number (e.g. 0.0.0.1234)   [ log_file ]" ; exit 99 ; fi
+if [[ ! $1 ]] ; then echo "usage:  $0  build_number (e.g. 0.0.0-1234)   [ log_file ]" ; exit 99 ; fi
 
 LOG='2>&1 | egrep -v '\'\('[0-9]+/[0-9]+K ?'\)\''+'
 
@@ -13,8 +13,8 @@ REVISION=$1
 
 pushd ${THIS_DIR}  2>&1 > /dev/null
 
-  echo mvn --settings ./settings.xml --quiet -DREVISION=${REVISION} prepare-package package  $LOG | bash
-# echo mvn --settings ./settings.xml --debug -DREVISION=${REVISION} prepare-package package  $LOG | bash
+  echo mvn --settings ./settings.xml --quiet -DREVISION=${REVISION} clean prepare-package package  $LOG | bash
+# echo mvn --settings ./settings.xml --debug -DREVISION=${REVISION} clean prepare-package package  $LOG | bash
 
 popd               2>&1 > /dev/null
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ done making android_zipfile
