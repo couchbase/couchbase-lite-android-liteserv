f6e6c5746caa3db36c7623cce2165856597073ab
Phil Labee <philip@couchbase.com>
2014-05-01 13:35:47 -0700

    CBLT-273:  add some DEBUG output
    Change-Id: I42b6033d6903a89116564de9b64d353057b0b723

--- release/zip_jars.sh
+++ release/zip_jars.sh
@@ -13,8 +13,8 @@ REVISION=$1
 
 pushd ${THIS_DIR}  2>&1 > /dev/null
 
-  echo mvn --settings ./settings.xml --quiet -DREVISION=${REVISION} clean prepare-package package  $LOG | bash
-# echo mvn --settings ./settings.xml --debug -DREVISION=${REVISION} clean prepare-package package  $LOG | bash
+# echo mvn --settings ./settings.xml --quiet -DREVISION=${REVISION} clean prepare-package package  $LOG | bash
+  echo mvn --settings ./settings.xml --debug -DREVISION=${REVISION} clean prepare-package package  $LOG | bash
 
 popd               2>&1 > /dev/null
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ done making android_zipfile
