86ae917b232c926917048fe40a6d8fe0b75c0a74
hideki <hideki@couchbase.com>
2016-04-28 15:25:03 -0700

    Fixed #34 - Enable parameterized gradle builds
    - Parameter should be set like `./gradlew -Dversion=0.0.0-688 -Dstorage=SQLite | ForestDB <task>`
    - For storage type, gradle set value in AndroidManifest.xml, at runtime, LiteServ gets storage type from Manifest.xml.

--- couchbase-lite-android-liteserv/src/main/java/com/couchbase/liteservandroid/MainActivity.java
+++ couchbase-lite-android-liteserv/src/main/java/com/couchbase/liteservandroid/MainActivity.java
@@ -1,6 +1,8 @@
 package com.couchbase.liteservandroid;
 
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -19,27 +21,31 @@ import com.couchbase.lite.listener.LiteListener;
 import java.io.IOException;
 
 public class MainActivity extends Activity {
-
     private static final int DEFAULT_LISTEN_PORT = 5984;
     private static final String DATABASE_NAME = "cblite-test";
     private static final String LISTEN_PORT_PARAM_NAME = "listen_port";
-
     private static final String LISTEN_LOGIN_PARAM_NAME = "username";
     private static final String LISTEN_PASSWORD_PARAM_NAME = "password";
-
-    private static final String STORAGE_TYPE_PARAM_NAME = "storage_type";
-    private static final String STORAGE_TYPE_FORESTDB = "ForestDB";
-    private static final String STORAGE_TYPE_SQLITE = "SQLite";
-
-
     public static String TAG = "LiteServ";
+
     private Credentials allowedCredentials;
-    private String storageType = STORAGE_TYPE_SQLITE;
+    private String storageType = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // storage
+        ApplicationInfo info = null;
+        try {
+            info = getApplicationContext().getPackageManager().getApplicationInfo(getApplicationContext().getPackageName(), PackageManager.GET_META_DATA);
+            storageType = (String)info.metaData.get("storage");
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "failed to obtain meta data from AndroidManifest.xml", e);
+            storageType = "sqlite";
+        }
+        Log.i(TAG, "storageType=" + storageType);
 
         // Register the JavaScript view compiler
         View.setCompiler(new JavaScriptViewCompiler());
@@ -50,7 +56,7 @@ public class MainActivity extends Activity {
             showListenPort(port);
             showListenCredentials();
         } catch (Exception e) {
-            TextView listenPortTextView = (TextView)findViewById(R.id.listen_port_textview);
+            TextView listenPortTextView = (TextView) findViewById(R.id.listen_port_textview);
             listenPortTextView.setText(String.format("Error starting LiteServ"));
             Log.e(TAG, "Error starting LiteServ", e);
         }
@@ -58,12 +64,12 @@ public class MainActivity extends Activity {
 
     private void showListenPort(int listenPort) {
         Log.d(TAG, "listenPort: " + listenPort);
-        TextView listenPortTextView = (TextView)findViewById(R.id.listen_port_textview);
+        TextView listenPortTextView = (TextView) findViewById(R.id.listen_port_textview);
         listenPortTextView.setText(String.format("Listening on port: %d.  Db: %s Storage: %s", listenPort, DATABASE_NAME, storageType));
     }
 
     private void showListenCredentials() {
-        TextView listenCredentialsTextView = (TextView)findViewById(R.id.listen_credentials_textview);
+        TextView listenCredentialsTextView = (TextView) findViewById(R.id.listen_credentials_textview);
         String credentialsDisplay = String.format(
                 "login: %s password: %s",
                 allowedCredentials.getLogin(),
@@ -71,43 +77,32 @@ public class MainActivity extends Activity {
         );
         Log.v(TAG, credentialsDisplay);
         listenCredentialsTextView.setText(credentialsDisplay);
-
     }
 
     private int startCBLListener(int suggestedListenPort) throws IOException, CouchbaseLiteException {
-
         Manager manager = startCBLite();
         startDatabase(manager, DATABASE_NAME);
 
-        if (getLogin()!=null && getPassword()!=null){
-            if(getLogin().equals("none") && getPassword().equals("none")) {
+        if (getLogin() != null && getPassword() != null) {
+            if (getLogin().equals("none") && getPassword().equals("none")) {
                 allowedCredentials = new Credentials("", "");
             } else {
                 allowedCredentials = new Credentials(getLogin(), getPassword());
             }
-        }  else {
+        } else {
             allowedCredentials = new Credentials();
         }
 
         LiteListener listener = new LiteListener(manager, suggestedListenPort, allowedCredentials);
-
         int port = listener.getListenPort();
         Thread thread = new Thread(listener);
         thread.start();
-
         return port;
     }
 
     protected Manager startCBLite() throws IOException {
-        Manager manager;
-        manager = new Manager(new AndroidContext(this), Manager.DEFAULT_OPTIONS);
-        this.storageType = getStorageType();
-        if (storageType != null && storageType.compareToIgnoreCase(STORAGE_TYPE_FORESTDB) == 0)
-            this.storageType = Manager.FORESTDB_STORAGE;
-        else
-            this.storageType = Manager.SQLITE_STORAGE;
-        Log.i(TAG, "storageType: " + this.storageType);
-        manager.setStorageType(this.storageType);
+        Manager manager = new Manager(new AndroidContext(this), Manager.DEFAULT_OPTIONS);
+        manager.setStorageType(storageType);
         return manager;
     }
 
@@ -128,15 +123,10 @@ public class MainActivity extends Activity {
         return getIntent().getStringExtra(LISTEN_PASSWORD_PARAM_NAME);
     }
 
-    private String getStorageType() {
-        return getIntent().getStringExtra(STORAGE_TYPE_PARAM_NAME);
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
 }
